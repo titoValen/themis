6e6a83f7012daff6b9b25aa0bb80aed95bd90e7a
titoValen <valentin.tito@davinci.edu.ar>
2026-09-02 01:30:04 -0300

    feat: agregar configuración para VitePWA en vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+// import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    /*VitePWA({
+      registerType: "",
+      manifest: {
+        name: "",
+        short_name: "",
+        description: "",
+        theme_color: "",
+        icons: [
+          {
+            src: "",
+            sizes: "",
+            type: ""
+          }
+        ]
+      }
+    })*/
+  ],
 })
